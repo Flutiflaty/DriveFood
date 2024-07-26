75c60718528f2ad5d947df408a7c9603657e241e
anakmenezes <anakmenezes@outlook.com>
2024-07-26 10:31:29 -0300

    aula-4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ print('D̲r̲i̲v̲e̲F̲o̲o̲d̲\n')
 print('1. Cadastrar restaurante\n2. Listar restaurantes\n3. Ativar restaurante\n4. Sair do app\n')
 opcao_escolhida = int(input('Escolha uma opção: '))
 
-def encerrando_app():
+def encerrando_apps():
     os.system('cls')
     print('encerrando app\n')
 
@@ -15,7 +15,7 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Insira as informações abaixo para ativarmos seu restaurante\n') 
 else: 
-    encerrando_app
+    encerrando_apps
 
 
 
